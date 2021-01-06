boundarydevices/linux

pci: dwc: pci-imx6: set or clear IMX8MQ_GPR_PCIE_REF_USE_PAD appropriately
Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

drivers/pci/dwc/pci-imx6.c
@@ -1280,10 +1280,10 @@ static void imx_pcie_init_phy(struct imx_pcie *imx_pcie)
 		else
 			val = IOMUXC_GPR16;
 
+		regmap_update_bits(imx_pcie->iomuxc_gpr, val,
+			IMX8MQ_GPR_PCIE_REF_USE_PAD,
+			(imx_pcie->ext_osc) ? IMX8MQ_GPR_PCIE_REF_USE_PAD : 0);
 		if (imx_pcie->ext_osc) {
-			regmap_update_bits(imx_pcie->iomuxc_gpr, val,
-					IMX8MQ_GPR_PCIE_REF_USE_PAD,
-					IMX8MQ_GPR_PCIE_REF_USE_PAD);
 			if (imx_pcie->variant == IMX8MM) {
 				dev_info(imx_pcie->pci->dev,
 					"Initialize PHY with EXT REfCLK!.\n");

shitalkumar-gandhi/linux-imx6-1

drivers/pci/controller/dwc/pci-imx6.c
@@ -1591,9 +1591,10 @@ static void imx6_pcie_init_phy(struct imx6_pcie *imx6_pcie)
 
 		dev_info(imx6_pcie->pci->dev, "%s REF_CLK is used!.\n",
 			 imx6_pcie->ext_osc ? "EXT" : "PLL");
+		regmap_update_bits(imx6_pcie->iomuxc_gpr, offset,
+			IMX8MQ_GPR_PCIE_REF_USE_PAD,
+			(imx6_pcie->ext_osc) ? IMX8MQ_GPR_PCIE_REF_USE_PAD : 0);
 		if (imx6_pcie->ext_osc) {
-			regmap_update_bits(imx6_pcie->iomuxc_gpr, offset,
-					   IMX8MQ_GPR_PCIE_REF_USE_PAD, 0);
 			regmap_update_bits(imx6_pcie->iomuxc_gpr, offset,
 					   IMX8MM_GPR_PCIE_REF_CLK_SEL,
 					   IMX8MM_GPR_PCIE_REF_CLK_SEL);
@@ -1615,8 +1616,6 @@ static void imx6_pcie_init_phy(struct imx6_pcie *imx6_pcie)
 			udelay(200);
 		} else {
 			/* Configure the internal PLL as REF clock */
-			regmap_update_bits(imx6_pcie->iomuxc_gpr, offset,
-					   IMX8MQ_GPR_PCIE_REF_USE_PAD, 0);
 			regmap_update_bits(imx6_pcie->iomuxc_gpr, offset,
 					   IMX8MM_GPR_PCIE_REF_CLK_SEL,
 					   IMX8MM_GPR_PCIE_REF_CLK_SEL);
@@ -1662,7 +1661,8 @@ static void imx6_pcie_init_phy(struct imx6_pcie *imx6_pcie)
 		regmap_update_bits(imx6_pcie->iomuxc_gpr,
 				   imx6_pcie_grp_offset(imx6_pcie),
 				   IMX8MQ_GPR_PCIE_REF_USE_PAD,
-				   IMX8MQ_GPR_PCIE_REF_USE_PAD);
+				   (imx6_pcie->ext_osc) ?
+					IMX8MQ_GPR_PCIE_REF_USE_PAD : 0);
 		break;
 	case IMX8MP:
 	case IMX8MP_EP: