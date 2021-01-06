boundarydevices/linux

of_mdio: reduce noise when scanning for phy address
Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>

drivers/of/of_mdio.c
@@ -170,12 +170,12 @@ int of_mdiobus_register(struct mii_bus *mdio, struct device_node *np)
 				continue;
 
 			/* be noisy to encourage people to set reg property */
-			dev_info(&mdio->dev, "scan phy %s at address %i\n",
+			dev_dbg(&mdio->dev, "scan phy %s at address %i\n",
 				 child->name, addr);
 
 			rc = of_mdiobus_register_phy(mdio, child, addr);
-			if (rc)
-				continue;
+			if (!rc)
+				break;
 		}
 	}
 

shitalkumar-gandhi/linux-imx6-1

This also fixes upstream commit
209c65b61d94344522c41a83cd6ce51aac5fd0a4
drivers/of/of_mdio.c:fix of_mdiobus_register()
Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

drivers/of/of_mdio.c
@@ -261,7 +261,7 @@ int of_mdiobus_register(struct mii_bus *mdio, struct device_node *np)
 				continue;
 
 			/* be noisy to encourage people to set reg property */
-			dev_info(&mdio->dev, "scan phy %pOFn at address %i\n",
+			dev_dbg(&mdio->dev, "scan phy %pOFn at address %i\n",
 				 child, addr);
 
 			if (of_mdiobus_child_is_phy(child)) {